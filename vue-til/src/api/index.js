777e8c33431a87dd2a5b20362d624f73352dc7d2
Jin <khjkhj1212@hanmail.net>
2021-09-23 17:25:15 +0900

    [추가] env 및 axios 설정
    설정파일 추가

--- vue-til/src/api/index.js
+++ vue-til/src/api/index.js
@@ -1,8 +1,13 @@
 import axios from "axios";
 
+const instance = axios.create({
+  baseURL: process.env.VUE_APP_API_URL,
+});
+
 function registerUser(data) {
-  const url = "http://localhost:8090/member/insert";
-  return axios.post(url, data);
+  // const url = "http://localhost:8090/member/insert";
+  // return axios.post(url, data);
+  return instance.post("/member/insert", data);
 }
 
 export { registerUser };
